2e1a1ca21b517abcb87197f1eefa2d1522eccb53
labesalena <alenalabes@gmail.com>
2017-02-21 19:44:09 +0300

    add test1

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,9 @@ public class Main {
     public static void main (String[] args) {
         System.out.println("Hello, World");
         Test test = new Test();
+        Test1 test1 = new Test1();
+
         System.out.println(test.getA());
-        System.out.println(test.getA());
+        System.out.println(test1.b);
     }
 }
---
+++ src/Test1.java
@@ -0,0 +1,6 @@
+/**
+ * Created by ilya on 21.02.2017.
+ */
+public class Test1 {
+    int b;
+}
